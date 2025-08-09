8ceec0c64fe0301dfd712430daee8c6e5ba6057d
Kayky de Brito <oss@kaykyb.com>
2025-08-09 05:58:05 -0300

    Make card leque look like an arc

--- src/components/CardLeque.tsx
+++ src/components/CardLeque.tsx
@@ -7,10 +7,52 @@ interface Props {
 
 export default function CardLeque({ cards }: Props) {
   return (
-    <div className="fixed -bottom-16 w-full flex justify-center gap-4">
-      {cards.map((card, i) => (
-        <DraggableCard card={card} key={i} />
-      ))}
+    <div className="fixed -bottom-32 left-0 right-0 w-full">
+      <div className="relative mx-auto h-64 w-full max-w-4xl">
+        {cards.map((card, i) => {
+          const total = cards.length;
+
+          type Pos = { top: number; dx: number; rotate: number };
+          const presets: Record<number, Pos[]> = {
+            1: [{ top: 24, dx: 0, rotate: 0 }],
+            2: [
+              { top: 20, dx: -70, rotate: -8 },
+              { top: 28, dx: 70, rotate: 8 },
+            ],
+            3: [
+              { top: 28, dx: -110, rotate: -15 },
+              { top: 16, dx: 0, rotate: 0 },
+              { top: 24, dx: 110, rotate: 15 },
+            ],
+            4: [
+              { top: 30, dx: -160, rotate: -20 },
+              { top: 20, dx: -55, rotate: -5 },
+              { top: 24, dx: 55, rotate: 10 },
+              { top: 34, dx: 160, rotate: 25 },
+            ],
+          };
+
+          const layout = presets[total as 1 | 2 | 3 | 4];
+          const pos: Pos = layout?.[i] ?? { top: 24, dx: (i - (total - 1) / 2) * 80, rotate: 0 };
+
+          const zIndex = 1000 - i;
+
+          return (
+            <div
+              key={i}
+              className="absolute"
+              style={{
+                left: "50%",
+                top: `${pos.top}px`,
+                transform: `translate(-50%, 0) translateX(${pos.dx}px) rotate(${pos.rotate}deg)`,
+                zIndex,
+              }}
+            >
+              <DraggableCard card={card} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
